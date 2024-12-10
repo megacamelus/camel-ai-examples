da762f335b60bacdbc36a41aeed0011faf0e3fbb
Tadayoshi Sato <sato.tadayoshi@gmail.com>
2024-12-10 17:30:28 +0900

    Add README for TorchServe examples

--- torchserve/metrics.java
+++ torchserve/metrics.java
@@ -1,5 +1,9 @@
 // camel-k: language=java dependency=camel:torchserve
 
+//DEPS org.apache.camel:camel-bom:4.9.0@pom
+//DEPS org.apache.camel:camel-core
+//DEPS org.apache.camel:camel-torchserve
+
 import org.apache.camel.builder.RouteBuilder;
 
 public class metrics extends RouteBuilder {
--- torchserve/predictions.java
+++ torchserve/predictions.java
@@ -1,5 +1,9 @@
 // camel-k: language=java dependency=camel:torchserve
 
+//DEPS org.apache.camel:camel-bom:4.9.0@pom
+//DEPS org.apache.camel:camel-core
+//DEPS org.apache.camel:camel-torchserve
+
 import org.apache.camel.builder.RouteBuilder;
 
 public class predictions extends RouteBuilder {
@@ -7,8 +11,8 @@ public class predictions extends RouteBuilder {
     @Override
     public void configure() throws Exception {
         // @formatter:off
-        from("file:data/objects?noop=true&recursive=true&include=.*\\.(jpg|png)")
-            .to("torchserve:inference/predictions?modelName=squeezenet1_1")
+        from("file:data/mnist?noop=true&recursive=true&include=.*\\.png")
+            .to("torchserve:inference/predictions?modelName=mnist_v2")
             .log("${headers.camelFileName} => ${body}");
         // @formatter:on
     }
